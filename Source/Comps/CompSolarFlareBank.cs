897abff47e08566281449607575e42e6cce44eae
BlueEagle421 <joachim.wielewski@gmail.com>
2025-06-29 13:31:35 +0200

    Add solar flare bank (xml, code, tex, lang)

---
+++ Source/Comps/CompSolarFlareBank.cs
@@ -0,0 +1,134 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace USH_GE
+{
+    public class MapComponent_SolarFlareBank(Map map) : MapComponent(map)
+    {
+        private readonly HashSet<CompSolarFlareBank> _solarBanks = [];
+        public HashSet<CompSolarFlareBank> AllSolarBanks => _solarBanks;
+        public void Register(CompSolarFlareBank comp) => _solarBanks.Add(comp);
+        public void Unregister(CompSolarFlareBank comp) => _solarBanks.Remove(comp);
+    }
+
+    public class CompProperties_SolarFlareBank : CompProperties_Power
+    {
+        public int dischargeTicks = 60000 * 15; // 1 day * 10
+        public CompProperties_SolarFlareBank() => compClass = typeof(CompSolarFlareBank);
+    }
+
+
+    [StaticConstructorOnStartup]
+    public class CompSolarFlareBank : CompPowerPlant
+    {
+        public CompProperties_SolarFlareBank BankProps => (CompProperties_SolarFlareBank)props;
+        private static readonly Vector2 BarSize = new(1.2f, 0.14f);
+        private static readonly Material PowerPlantSolarBarFilledMat = SolidColorMaterials.SimpleSolidColorMaterial(new(0.5f, 0.475f, 0.1f));
+        private static readonly Material PowerPlantSolarBarUnfilledMat = SolidColorMaterials.SimpleSolidColorMaterial(new(0.15f, 0.15f, 0.15f));
+
+        private bool _isDischarging;
+        private int _dischargeTicksLeft = 0;
+        private const int DISCHARGE_INTERVAL = 2000;
+        private const float Z_OFFSET = .018292684f;
+        private const float Y_OFFSET = 0.5f;
+
+        public virtual void Notify_SolarFlareIntercepted()
+        {
+            StartProduction();
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+
+            if (parent.IsHashIntervalTick(DISCHARGE_INTERVAL))
+                _dischargeTicksLeft -= DISCHARGE_INTERVAL;
+
+            if (_dischargeTicksLeft <= 0)
+                StopGenerating();
+        }
+
+        private void StartProduction()
+        {
+            _isDischarging = true;
+            _dischargeTicksLeft = BankProps.dischargeTicks;
+        }
+
+        private void StopGenerating()
+        {
+            _isDischarging = false;
+            _dischargeTicksLeft = 0;
+        }
+
+        protected override float DesiredPowerOutput
+        {
+            get
+            {
+                if (!_isDischarging)
+                    return 0;
+
+                return _dischargeTicksLeft / (float)BankProps.dischargeTicks * -Props.PowerConsumption;
+            }
+        }
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+
+            parent.Map.GetComponent<MapComponent_SolarFlareBank>().Register(this);
+        }
+
+        public override void PostDeSpawn(Map map, DestroyMode mode = DestroyMode.Vanish)
+        {
+            map.GetComponent<MapComponent_SolarFlareBank>().Unregister(this);
+            base.PostDeSpawn(map, mode);
+        }
+
+        public override void PostDraw()
+        {
+            base.PostDraw();
+
+            DrawBar();
+        }
+
+        private void DrawBar()
+        {
+            if (!_isDischarging)
+                return;
+
+            GenDraw.FillableBarRequest r = new()
+            {
+                center = parent.DrawPos + Vector3.up * Z_OFFSET + Vector3.forward * Y_OFFSET,
+                size = BarSize,
+                fillPercent = PowerOutput / (0f - Props.PowerConsumption),
+                filledMat = PowerPlantSolarBarFilledMat,
+                unfilledMat = PowerPlantSolarBarUnfilledMat,
+                margin = 0.15f
+            };
+            Rot4 rotation = parent.Rotation;
+            rotation.Rotate(RotationDirection.Clockwise);
+            r.rotation = rotation;
+            GenDraw.DrawFillableBar(r);
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+
+            Scribe_Values.Look(ref _isDischarging, "_isDischarging");
+            Scribe_Values.Look(ref _dischargeTicksLeft, "_dischargeTicksLeft");
+        }
+
+        public override string CompInspectStringExtra()
+        {
+            if (_isDischarging)
+                return base.CompInspectStringExtra() + "\n" + "USH_GE_DischargeTimeLeft".Translate(_dischargeTicksLeft.ToStringTicksToPeriod());
+
+            return base.CompInspectStringExtra();
+        }
+    }
+
+}
---
+++ Source/Harmony/PatchGameConditionManagerRegisterCondition.cs
@@ -0,0 +1,41 @@
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace USH_GE
+{
+    [HarmonyPatch(typeof(GameConditionManager), nameof(GameConditionManager.RegisterCondition))]
+    public static class GameConditionManager_RegisterCondition
+    {
+        private const int INTERCEPT_LETTER_DELAY = 60;
+
+        [HarmonyPrefix]
+        public static bool Prefix(
+            GameConditionManager __instance,
+            GameCondition cond)
+        {
+            if (cond.def == IncidentDefOf.SolarFlare.gameCondition)
+            {
+                var component = __instance.ownerMap.GetComponent<MapComponent_SolarFlareBank>();
+
+                if (!component.AllSolarBanks.NullOrEmpty())
+                {
+                    InterceptSolarFlare(component);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void InterceptSolarFlare(MapComponent_SolarFlareBank component)
+        {
+            foreach (var bank in component.AllSolarBanks)
+                bank.Notify_SolarFlareIntercepted();
+
+            string label = "USH_GE_SolarFlareInterceptedLabel".Translate();
+            string text = "USH_GE_SolarFlareInterceptedText".Translate();
+            Find.LetterStack.ReceiveLetter(label, text, LetterDefOf.PositiveEvent, null, INTERCEPT_LETTER_DELAY);
+        }
+    }
+}
