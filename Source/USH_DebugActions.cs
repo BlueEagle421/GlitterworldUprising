35f682af3845cae5061a343fa9b33b6b40625691
BlueEagle421 <joachim.wielewski@gmail.com>
2025-07-13 18:17:55 +0200

    Clean up glittership code, add debug action

---
+++ Source/USH_DebugActions.cs
@@ -0,0 +1,9 @@
+using LudeonTK;
+
+namespace USH_GE;
+
+public static class USH_DebugActions
+{
+    [DebugAction("Ushankas", "Spawn glittertech chunk")]
+    public static void DebugSpawnChunk() => WorldComponent_GlittershipChunk.Instance.EventSpawnChunk();
+}
--- Source/WorldComponent_GlittershipChunk.cs
+++ Source/WorldComponent_GlittershipChunk.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using LudeonTK;
 using RimWorld;
 using RimWorld.Planet;
 using Verse;
@@ -11,6 +12,7 @@ public class WorldComponent_GlittershipChunk : WorldComponent
     public static WorldComponent_GlittershipChunk Instance { get; private set; }
     public WorldComponent_GlittershipChunk(World world) : base(world)
     {
+        Instance = this;
         _ticksToFire = _tickDelayRange.RandomInRange;
     }
 
@@ -48,18 +50,7 @@ public class WorldComponent_GlittershipChunk : WorldComponent
         if (map == null)
             return;
 
-        if (TryFindShipChunkDropCell(map.Center, map, 999999, out var pos))
-        {
-            var listerThings = Find.CurrentMap.listerThings;
-            List<Thing> allTargets = listerThings.ThingsOfDef(ThingDefOf.SimpleResearchBench);
-            allTargets.AddRange(listerThings.ThingsOfDef(USH_DefOf.USH_ResearchProbe));
-            allTargets.AddRange(listerThings.ThingsOfDef(USH_DefOf.HiTechResearchBench));
-
-            Thing firstTarget = allTargets.First();
-            pos = firstTarget.Position;
-        }
-
-        SpawnChunk(pos, map);
+        EventSpawnChunk();
 
         string label = "USH_GE_LetterLabelGlittershipChunk".Translate();
         string desc = "USH_GE_LetterGlittershipChunk".Translate();
@@ -67,6 +58,40 @@ public class WorldComponent_GlittershipChunk : WorldComponent
 
         _didEvent = true;
     }
+
+    public void EventSpawnChunk()
+    {
+        Map map = Find.AnyPlayerHomeMap;
+
+        if (map == null)
+            return;
+
+        if (!TryFindShipChunkDropCell(map.Center, map, 999999, out var pos))
+        {
+            List<ThingDef> potentialSpawnDefs =
+            [
+                ThingDefOf.Bed,
+                ThingDefOf.SimpleResearchBench,
+                USH_DefOf.HiTechResearchBench,
+                USH_DefOf.USH_ResearchProbe,
+            ];
+
+            pos = FindThingsOfDef(potentialSpawnDefs).First().Position;
+        }
+
+        SpawnChunk(pos, map);
+    }
+
+    private List<Thing> FindThingsOfDef(List<ThingDef> defs)
+    {
+        var listerThings = Find.CurrentMap.listerThings;
+        List<Thing> result = [];
+
+        defs.ForEach(x => result.AddRange(listerThings.ThingsOfDef(x)));
+        return result;
+    }
+
+
     private void SpawnChunk(IntVec3 pos, Map map)
     {
         ThingDef skyfaller = USH_DefOf.USH_GlittershipChunkIncoming;
@@ -75,14 +100,12 @@ public class WorldComponent_GlittershipChunk : WorldComponent
         SkyfallerMaker.SpawnSkyfaller(skyfaller, chunk, pos, map);
     }
 
-    private bool TryFindShipChunkDropCell(IntVec3 nearLoc, Map map, int maxDist, out IntVec3 pos)
+    private static bool TryFindShipChunkDropCell(IntVec3 nearLoc, Map map, int maxDist, out IntVec3 pos)
     {
-        ThingDef skyfaller = USH_DefOf.USH_GlittershipChunkIncoming;
-
         return CellFinderLoose.TryFindSkyfallerCell(
-            skyfaller,
+            ThingDefOf.ShipChunkIncoming,
             map,
-            skyfaller.terrainAffordanceNeeded,
+            ThingDefOf.ShipChunk.terrainAffordanceNeeded,
             out pos,
             10,
             nearLoc,
