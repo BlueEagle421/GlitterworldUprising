489cd2d9171df1e0eb824ebf90f9bc692b51c83c
BlueEagle421 <joachim.wielewski@gmail.com>
2025-07-18 21:52:29 +0200

    Fix glittercrate spawning

--- Source/Harmony/PatchAncientShrine_Resolve.cs
+++ Source/Harmony/PatchAncientShrine_Resolve.cs
@@ -1,27 +1,17 @@
 using HarmonyLib;
 using RimWorld.BaseGen;
 using USH_GE;
-using Verse;
 
-[HarmonyPatch(typeof(SymbolResolver_AncientShrine), "Resolve")]
-public static class Patch_SymbolResolver_AncientShrine_Resolve
+[HarmonyPatch(typeof(SymbolResolver_Interior_AncientTemple), "Resolve")]
+public static class Patch_SymbolResolver_Interior_AncientTemple_Resolve
 {
-    private static bool _crateSpawned;
-
     public static void Postfix(ResolveParams rp)
     {
-        if (_crateSpawned)
-            return;
-
         if (rp.rect == null)
             return;
 
         ResolveParams crateParams = rp;
         crateParams.singleThingDef = USH_DefOf.USH_Glittercrate;
-        crateParams.thingRot = Rot4.North;
-
         BaseGen.symbolStack.Push("thing", crateParams);
-
-        _crateSpawned = true;
     }
 }
