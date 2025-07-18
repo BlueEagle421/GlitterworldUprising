2ce393de67808c81d3d7c7df53fc5ce8529bafef
BlueEagle421 <joachim.wielewski@gmail.com>
2025-07-18 15:52:45 +0200

    Fix debug actions, update dll

--- Source/USH_DebugActions.cs
+++ Source/USH_DebugActions.cs
@@ -5,5 +5,5 @@ namespace USH_GE;
 public static class USH_DebugActions
 {
     [DebugAction("Ushankas", "Spawn glittertech chunk")]
-    public static void DebugSpawnChunk() => WorldComponent_GlittershipChunk.Instance.EventSpawnChunk();
+    public static void DebugSpawnChunk() => WorldComponent_GlittershipChunk.Instance.FireEvent();
 }
